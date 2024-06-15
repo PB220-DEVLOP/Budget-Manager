d35f99eb0ac298353c29d89a71c321bf233d9079
“codemonic-zero” <piyushmfalke@gmail.com>
2024-06-15 16:15:32 +0530

    "video's changes to giffy"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import video1 from '../assets/1.mp4';
-import video2 from '../assets/2.mp4';
-import video3 from '../assets/3.mp4';
+import giffy from '../assets/bgtmnt.gif';
 import img1 from '../assets/img_sreen_light1.png';
 import img2 from '../assets/img_sreen_light2.png';
 import img3 from '../assets/img_sreen_light3.png';
@@ -24,33 +22,10 @@ const HomePage = () => {
         <div className="mb-8">
           <Carousel>
             <Carousel.Item>
-              <video className="d-block w-100" controls autoPlay loop>
-                <source src={video1} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
+              <img src={giffy} className="d-block w-100" alt="gif1"/>
               <Carousel.Caption>
                 <h3>First slide label</h3>
                 <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <video className="d-block w-100" controls autoPlay loop>
-                <source src={video2} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <video className="d-block w-100" controls autoPlay loop>
-                <source src={video3} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
