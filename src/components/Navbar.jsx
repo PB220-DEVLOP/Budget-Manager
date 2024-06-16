e8fdbb76e92fb5150eedb1652755eeae9846cc09
“codemonic-zero” <piyushmfalke@gmail.com>
2024-06-16 16:31:02 +0530

    "all problems solved"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
             </NavLink>
           </li>
           <li className="mb-4">
-            <NavLink to="/about-us" className="flex items-center p-2 rounded hover:bg-gray-700" activeClassName="bg-gray-700">
+            <NavLink to="/faq" className="flex items-center p-2 rounded hover:bg-gray-700" activeClassName="bg-gray-700">
               <FaInfoCircle className="mr-3" />
               FAQ's(Frequently Asked Questions)
             </NavLink>
